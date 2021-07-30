531a147f2a70697f2a73d55eac414c64d56d48e9
Advik Singh Somvanshi <advik@criptext.com>
2021-07-30 21:44:35 +0530

    Update and rename test4.py to marketdetails.py

--- marketdetails.py
+++ marketdetails.py
@@ -1,7 +1,7 @@
 import requests
-import pprint
 
 coindata = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d')
+
 # For no.1 coin
 name1 = str(coindata.json()[0]['name'])
 priceusd1 = str(coindata.json()[0]['current_price'])
