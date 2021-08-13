18f9d8d9cc7734b5ec3cf204e17b34bb1d9c7782
Advik Singh Somvanshi <advik@criptext.com>
2021-08-13 18:29:04 +0530

    Switched from webhook to polling
    Webhook requires knowledge of Flask, so...

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import telebot, random
 from telebot import types
 from time import sleep
-import os
-PORT = int(os.environ.get('PORT', 5000))
+import marketindex as mi
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-TOKEN='1922118154:AAGTrDR4jtfP7rEhszhQ4BsQWVNKpDj0Hpg'
-
-bot = telebot.TeleBot(TOKEN, parse_mode='MarkdownV2')
+bot = telebot.TeleBot('1922118154:AAGTrDR4jtfP7rEhszhQ4BsQWVNKpDj0Hpg', parse_mode='HTML')
 print("Bot started successfully! Running now...")
-user = bot.get_me()
 
 knwn = [] #Save UserIDs in a list
 with open('userids.txt', 'r') as uids:
@@ -30,7 +19,7 @@ def newmsg(messages):
         elif m.content_type in ['audio', 'video', 'document', 'sticker', 'location', 'voice', 'photo']:
             print(str(m.chat.first_name) + " sent a misc file")
 
-bot.set_update_listener(newmsg) #To turn on listener
+bot.set_update_listener(newmsg) #Turns on listener
 
 @bot.message_handler(commands=['start'])
 def wlcm_msg(message):
@@ -38,34 +27,43 @@ def wlcm_msg(message):
     ccid = str(cid)
     if ccid not in knwn:
         knwn.append(ccid)
-        bot.send_message(cid, "Ciao " + message.from_user.first_name + "\! I'mma *bot* made by @advik\_143\! \nI can perform various tasks, type /help to view all available commands & features")
+        bot.send_message(cid, "Ciao " + message.from_user.first_name + "! I'mma <b>bot</b> made by @advik_143! \nI can perform various tasks, type /help to view all available commands & features!")
         with open("userids.txt", 'w') as uids:
             for each_uid in knwn:
                 each_mod_uid = each_uid + ' '
                 uids.write(each_mod_uid)
         sleep(0.7)
-        bot.send_message(cid, "UserID saved\!")
+        bot.send_message(cid, "UserID saved!")
     else:
-        bot.send_message(cid, 'Heya ' + message.from_user.first_name + ', Welcome again\!')
+        bot.send_message(cid, 'Heya ' + message.from_user.first_name + ', Welcome again!')
 
 @bot.message_handler(commands=['anime', 'animes'])
 def animes_list(message):
     bot.reply_to(message, "List of good animes to watch:\nBlack clover, fire force, one piece, the duke of death and his maid, girlfriend girlfriend , JoJo \(if you haven't watched it\) , mob psycho 100 , one punch man , the rising of the shield hero , konosuba , that time i got reincarnated as a slime , slime diaries , grand blue , love chunibyo and other delusions , free\!dive , burning kabaddi , clean freak aoyama kun , haikyuu\nHava Nice day\!")
 
+@bot.message_handler(commands=['top10'])
+def index(message):
+    cid = message.chat.id
+    bot.send_message(cid, mi.getinfo())
+
 @bot.message_handler(commands=['help'])
 def all_cmds(message):
     bot.send_message(message.from_user.id, """🔰 *Following features:*
-Features list is still being updated, lmao get fcked\!""")
+1. Chatbot - Try saying Hi, hello, ciao or namaste
+2. Market Index - /top10 Get top 10 Cryptos by their market cap
+3. More features being added;)""")
+    
+    #Test feature, to be taken care later..
     '''mrkp = types.ReplyKeyboardMarkup(one_time_keyboard=True)
     btn1 = types.KeyboardButton('Hi')
     btn2 = types.KeyboardButton('Who tf are you?')
     mrkp.row(btn1, btn2)
-    bot.send_message(message.from_user.id, "Click one\!", reply_markup=mrkp)
+    bot.send_message(message.from_user.id, "Click one!", reply_markup=mrkp)
     hideboard = types.ReplyKeyboardRemove()'''
 
 @bot.message_handler(content_types=['location'])
 def doc_msg(message):
-    bot.reply_to(message, "I know your location now, I shall come & get you soon\!")
+    bot.reply_to(message, "I know your location now, I shall come & get you soon!")
 
 
 @bot.message_handler(content_types=['text'])
@@ -74,18 +72,13 @@ def hi_msg(message):
     hallo = message.text.lower()
 
     if hallo in ('hi', 'hey', 'hello', 'hola', 'halo'):
-        himsgs = ["Heyo, Wassup\?", "Halo\!", "Yo man, how's it going\?", "Hey\!"]
+        himsgs = ["Heyo, Wassup?", "Halo!", "Yo dude, how's it going?", "Hey!"]
         himsg = random.choice(himsgs)
         bot.send_message(cid, himsg)
     elif hallo in ('namaste', 'nmste', 'namste', 'nmaste'):
         bot.send_message(cid, "Namaste 🙏")
-    elif hallo in ('ciao'):
-        bot.send_message(cid, "Ciao bruh\!")
-    else:
-        bot.send_message(cid, "I don't get it what you mean:/")
+    elif hallo in ('ciao', 'ciao!', 'chao'):
+        bot.send_message(cid, "Ciao bruh!")
 
-        
-updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-updater.bot.setWebhook('https://advik143.herokuapp.com/' + TOKEN)
+
+bot.polling()
