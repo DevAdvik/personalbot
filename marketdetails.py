90ff7f59967e94b32115ab0c0e4c10f091d6e01b
Advik Singh Somvanshi <advik@criptext.com>
2021-08-13 18:32:08 +0530

    Update marketdetails.py

--- marketdetails.py
+++ marketdetails.py
@@ -7,46 +7,55 @@ name1 = str(coindata.json()[0]['name'])
 priceusd1 = str(coindata.json()[0]['current_price'])
 symbol1 = str(coindata.json()[0]['symbol']).upper()
 pricechange1 = str(coindata.json()[0]['price_change_percentage_24h'])[0:5]
+
 # For no.2 coin
 name2 = str(coindata.json()[1]['name'])
 priceusd2 = str(coindata.json()[1]['current_price'])
 symbol2 = str(coindata.json()[1]['symbol']).upper()
 pricechange2 = str(coindata.json()[1]['price_change_percentage_24h'])[0:5]
+
 # For no.3 coin
 name3 = str(coindata.json()[2]['name'])
 priceusd3 = str(coindata.json()[2]['current_price'])
 symbol3 = str(coindata.json()[2]['symbol']).upper()
 pricechange3 = str(coindata.json()[2]['price_change_percentage_24h'])[0:5]
+
 # For no.4 coin
 name4 = str(coindata.json()[3]['name'])
 priceusd4 = str(coindata.json()[3]['current_price'])
 symbol4 = str(coindata.json()[3]['symbol']).upper()
 pricechange4 = str(coindata.json()[3]['price_change_percentage_24h'])[0:5]
+
 # For no.5 coin
 name5 = str(coindata.json()[4]['name'])
 priceusd5 = str(coindata.json()[4]['current_price'])
 symbol5 = str(coindata.json()[4]['symbol']).upper()
 pricechange5 = str(coindata.json()[4]['price_change_percentage_24h'])[0:5]
+
 # For no.6 coin
 name6 = str(coindata.json()[5]['name'])
 priceusd6 = str(coindata.json()[5]['current_price'])
 symbol6 = str(coindata.json()[5]['symbol']).upper()
 pricechange6 = str(coindata.json()[5]['price_change_percentage_24h'])[0:5]
+
 # For no.7 coin
 name7 = str(coindata.json()[6]['name'])
 priceusd7 = str(coindata.json()[6]['current_price'])
 symbol7 = str(coindata.json()[6]['symbol']).upper()
 pricechange7 = str(coindata.json()[6]['price_change_percentage_24h'])[0:5]
+
 # For no.8 coin
 name8 = str(coindata.json()[7]['name'])
 priceusd8 = str(coindata.json()[7]['current_price'])
 symbol8 = str(coindata.json()[7]['symbol']).upper()
 pricechange8 = str(coindata.json()[7]['price_change_percentage_24h'])[0:5]
+
 # For no.9 coin
 name9 = str(coindata.json()[8]['name'])
 priceusd9 = str(coindata.json()[8]['current_price'])
 symbol9 = str(coindata.json()[8]['symbol']).upper()
 pricechange9 = str(coindata.json()[8]['price_change_percentage_24h'])[0:5]
+
 # For no.10 coin
 name10 = str(coindata.json()[9]['name'])
 priceusd10 = str(coindata.json()[9]['current_price'])
@@ -54,6 +63,7 @@ symbol10 = str(coindata.json()[9]['symbol']).upper()
 pricechange10 = str(coindata.json()[9]['price_change_percentage_24h'])[0:5]
 
 
+#Storing required details in a variable
 mdet1 = str("1. "+name1+"("+symbol1+"): $"+priceusd1+" ("+pricechange1+"%)")
 mdet2 = str("2. "+name2+"("+symbol2+"): $"+priceusd2+" ("+pricechange2+"%)")
 mdet3 = str("3. "+name3+"("+symbol3+"): $"+priceusd3+" ("+pricechange3+"%)")
@@ -66,5 +76,4 @@ mdet9 = str("9. "+name9+"("+symbol9+"): $"+priceusd9+" ("+pricechange9+"%)")
 mdet10 = str("10. "+name10+"("+symbol10+"): $"+priceusd10+" ("+pricechange10+"%)")
 
 marketdetails = mdet1+'\n'+mdet2+'\n'+mdet3+'\n'+mdet4+'\n'+mdet5+'\n'+mdet6+'\n'+mdet7+'\n'+mdet8+'\n'+mdet9+'\n'+mdet10
-
-#print(marketdetails)
+#marketdetails variable now hold info about Top10 Crypto Currencies
